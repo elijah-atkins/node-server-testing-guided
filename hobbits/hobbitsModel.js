6fce9b264475520825fe2a3f4e23608c80e484b7
Elijah Atkins <elijahorama@gmail.com>
2020-08-13 18:46:32 -0600

    class notes

--- hobbits/hobbitsModel.js
+++ hobbits/hobbitsModel.js
@@ -10,6 +10,7 @@ module.exports = {
 
 async function insert(hobbit) {
   const [id] = await db('hobbits').insert(hobbit);
+
   return db('hobbits').where({ id }).first();
 }
 
--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,27 +1,40 @@
-const Hobbits = require('./hobbitsModel');
 const db = require('../data/dbConfig.js');
+const Hobbits = require('./hobbitsModel.js');
+
+//----------------------------------------------------------------------------//
+// 
+// See ./api/server.spec.js for some info on the jest methods etc. 
+// 
+//----------------------------------------------------------------------------//
 
 describe('hobbits model', () => {
-    describe('insert', () => {
-        beforeEach(async () => {
-            await db('hobbits').truncate()
-        })
-        it('should insert to provided hobbits into the db', async () => {
-            await Hobbits.insert({name:'frodo'});
-            await Hobbits.insert({name: 'sam'});
+    describe('insert()', () => {
+        it('should insert the provided hobbits into the DB', async () => {
+            await Hobbits.insert({ name: 'gaffer' });
+            await Hobbits.insert({ name: 'sam' });
 
             const hobbits = await db('hobbits');
             expect(hobbits).toHaveLength(2);
-        })
-        it('should return the hobbit we inserted', async () => {
-            let hobbit = await Hobbits.insert({name:'bilbo'});
-            expect(hobbit.name).toBe('bilbo');
+        });
 
-            hobbit = await Hobbits.insert({name: 'gaffer'});
-            expect(hobbit.name).toBe('gaffer')
+        it('should return what was inserted', async () => {
+            let hobbit = await Hobbits.insert({ name: 'gaffer' });
+            expect(hobbit.name).toBe('gaffer');
+
+            hobbit = await Hobbits.insert({ name: 'sam' });
+            expect(hobbit.name).toBe('sam');
+        });
+
+        //----------------------------------------------------------------------------//
+        // 
+        // jest.beforeEach() specifies a method that is executed before each test. You
+        // would use this to do any setup or value initialization needed before *every*
+        // test.
+        // 
+        //----------------------------------------------------------------------------//
+
+        beforeEach(async () => {
+            await db('hobbits').truncate();
         })
-        it('is pointless', () => {
-            expect(true).toBe(true);
-        })
-    }) 
-})
+    });
+});
